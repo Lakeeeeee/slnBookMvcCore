619e4902899a137ac3b541afb726a339fa89024e
Lake <kmossrr5236@gmail.com>
2023-04-26 22:29:01 +0800

    04/28

--- prjBookMvcCore/Controllers/HomeController.cs
+++ prjBookMvcCore/Controllers/HomeController.cs
@@ -28,5 +28,11 @@ namespace prjBookMvcCore.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult MemberCenter()
+        {
+            return View();
+        }
+
     }
 }
