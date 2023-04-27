ac7cb287b470072a4c26943dd901c921ea0bac8e
shawnwei <hotel5782@hotmail.com>
2023-04-27 12:01:35 +0800

    solve image problem & create commentpage

--- prjBookMvcCore/Program.cs
+++ prjBookMvcCore/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=MemberCenter}/{id?}");
+    pattern: "{controller=Home}/{action=Home}/{id?}");
 
 app.Run();
