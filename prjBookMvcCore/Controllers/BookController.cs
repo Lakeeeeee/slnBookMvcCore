3e2312159a315b5110306e53cd58df3eb0e5c6c8
hcjohn463 <hcjohn463@gmail.com>
2023-05-01 17:38:04 +0800

    Add BookInformation
    書本資訊測試

---
+++ prjBookMvcCore/Controllers/BookController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjBookMvcCore.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult BookInformation()
+        {
+            return View();
+        }
+    }
+}
