390a08dfc2c9c23723fdfb54f51eed6558010f61
ShuyuWeng <wingy@apps.ntpc.edu.tw>
2023-05-03 17:32:35 +0800

    更新CaregoryController、menu

--- prjBookMvcCore/Controllers/CategoryController.cs
+++ prjBookMvcCore/Controllers/CategoryController.cs
@@ -13,27 +13,103 @@ namespace prjBookMvcCore.Controllers
                         select c;
             return View(datas);
         }
-        public IActionResult 語言學習()
+        public IActionResult 年齡()
         {
             IEnumerable<SubCategory> datas =db.SubCategories.Where(x=>x.CategoryId==29) ;
             return View(datas);
         }
+        public IActionResult 人文社科()
+        {
+            IEnumerable<SubCategory> datas = db.SubCategories.Where(x => x.CategoryId == 29);
+            return View(datas);
+        }
 
-        public IActionResult 漫畫圖文書()
+        public IActionResult 心理勵志()
         {
             IEnumerable<SubCategory> datas = db.SubCategories.Where(x => x.CategoryId == 28);
             return View(datas);
         }
 
-        public IActionResult 旅遊()
+        public IActionResult 文學小說()
         {
             IEnumerable<SubCategory> datas = db.SubCategories.Where(x => x.CategoryId ==23);
             return View(datas);
         }
 
-        public IActionResult 年齡()
+        public IActionResult 日中對照()
         {
             IEnumerable<SubCategory> datas = db.SubCategories.Where(x => x.CategoryId ==1);
+            return View(datas);
+        }
+
+        public IActionResult 生活風格()
+        {
+            IEnumerable<SubCategory> datas = db.SubCategories.Where(x => x.CategoryId == 1);
+            return View(datas);
+        }
+        public IActionResult 考試用書()
+        {
+            IEnumerable<SubCategory> datas = db.SubCategories.Where(x => x.CategoryId == 1);
+            return View(datas);
+        }
+        public IActionResult 自然科普()
+        {
+            IEnumerable<SubCategory> datas = db.SubCategories.Where(x => x.CategoryId == 1);
+            return View(datas);
+        }
+        public IActionResult 宗教命理()
+        {
+            IEnumerable<SubCategory> datas = db.SubCategories.Where(x => x.CategoryId == 1);
+            return View(datas);
+        }
+        public IActionResult 旅遊()
+        {
+            IEnumerable<SubCategory> datas = db.SubCategories.Where(x => x.CategoryId == 1);
+            return View(datas);
+        }
+        public IActionResult 商業理財()
+        {
+            IEnumerable<SubCategory> datas = db.SubCategories.Where(x => x.CategoryId == 1);
+            return View(datas);
+        }
+        public IActionResult 童書青少年文學()
+        {
+            IEnumerable<SubCategory> datas = db.SubCategories.Where(x => x.CategoryId == 1);
+            return View(datas);
+        }
+        public IActionResult 飲食()
+        {
+            IEnumerable<SubCategory> datas = db.SubCategories.Where(x => x.CategoryId == 1);
+            return View(datas);
+        }
+        public IActionResult 電腦資訊()
+        {
+            IEnumerable<SubCategory> datas = db.SubCategories.Where(x => x.CategoryId == 1);
+            return View(datas);
+        }
+        public IActionResult 漫畫圖文書()
+        {
+            IEnumerable<SubCategory> datas = db.SubCategories.Where(x => x.CategoryId == 1);
+            return View(datas);
+        }
+        public IActionResult 語言學習()
+        {
+            IEnumerable<SubCategory> datas = db.SubCategories.Where(x => x.CategoryId == 1);
+            return View(datas);
+        }
+        public IActionResult 親子教養()
+        {
+            IEnumerable<SubCategory> datas = db.SubCategories.Where(x => x.CategoryId == 1);
+            return View(datas);
+        }
+        public IActionResult 醫療保健()
+        {
+            IEnumerable<SubCategory> datas = db.SubCategories.Where(x => x.CategoryId == 1);
+            return View(datas);
+        }
+        public IActionResult 藝術設計()
+        {
+            IEnumerable<SubCategory> datas = db.SubCategories.Where(x => x.CategoryId == 1);
             return View(datas);
         }
     }
