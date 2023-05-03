bae00b5e0945e3e7e778f60ee6bb5ee1a81a97ec
Lake <kmossrr5236@gmail.com>
2023-05-03 14:57:36 +0800

    Revert "update memberLayout"
    This reverts commit 89ab46a3ea1319011db6db06cde62f0abb1ddf94.

--- prjBookMvcCore/Models/Order.cs
+++ prjBookMvcCore/Models/Order.cs
@@ -1,21 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace prjBookMvcCore.Models
 {
     public partial class Order
     {
+        public Order()
+        {
+            OrderDetails = new HashSet<OrderDetail>();
+        }
+
         public int OrderId { get; set; }
         public int MemberId { get; set; }
+        [DataType(DataType.Date)]
         public DateTime OrderDate { get; set; }
         public int ShipmentId { get; set; }
         public int PaymentId { get; set; }
         public int PayStatusId { get; set; }
         public int ShippingStatusId { get; set; }
+        public decimal? Freight { get; set; }
         public int? DiscountId { get; set; }
         public string ShipAddr { get; set; } = null!;
-        public string? CouponId { get; set; }
-        public decimal? DiscountAmount { get; set; }
-        public decimal? TotalPay { get; set; }
+
+        public virtual Discount? Discount { get; set; }
+        public virtual Member Member { get; set; } = null!;
+        public virtual PayStatus PayStatus { get; set; } = null!;
+        public virtual Payment Payment { get; set; } = null!;
+        public virtual Shipment Shipment { get; set; } = null!;
+        public virtual ShippingStatus ShippingStatus { get; set; } = null!;
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
     }
 }
