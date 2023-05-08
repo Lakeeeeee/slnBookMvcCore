36bd854e0b997c9d3a33f385c1cb162844a937fe
Lake <kmossrr5236@gmail.com>
2023-05-08 11:05:07 +0800

    update

---
+++ prjBookMvcCore/RECaptcha.cs
@@ -0,0 +1,10 @@
+﻿namespace prjBookMvcCore
+{
+    public class RECaptcha
+    {
+        public string Key = "6Lf0vO4lAAAAADrJVEDlc3YI0LtPnTKXUZkjtcv5";
+
+        public string Secret = "6Lf0vO4lAAAAADf2cKK832eCTjQCWMMQlX3lix_t";
+        public string Response { get; set; }
+    }
+}
