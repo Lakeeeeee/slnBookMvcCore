fc7d88c94cd67f76587a259e7d0af2c28e73b8e9
ShuyuWeng <wingy@apps.ntpc.edu.tw>
2023-05-09 21:11:09 +0800

    修改

--- prjBookMvcCore/Controllers/BookController.cs
+++ prjBookMvcCore/Controllers/BookController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using prjBookMvcCore.Models;
-using static prjBookMvcCore.Models.BooksCardModel;
+using static prjBookMvcCore.Models.BooksCardViewModel;
 
 namespace prjBookMvcCore.Controllers
 {
@@ -14,30 +14,53 @@ namespace prjBookMvcCore.Controllers
             return View();
         }
 
-
         public IActionResult _BookCardPartial(int? id)
         {
-            if (id == null)
-                return RedirectToAction("Home");
+                var db = new BookShopContext();
+            
+                // Get book information
+                var book = db.Books.FirstOrDefault(b => b.BookId == id);
+                if (book == null)
+                    return RedirectToAction("Home");
 
-            var db = new BooksCardModel();
-
-            var datas = db.Books
-                .Where(book => book.BookId == id)
-                .Include(book => book.Authors)
-                .Include(book => book.BookDiscounts)
-                .Select(book => new
+                // Get author information
+                var authorDetail = db.AuthorDetails.FirstOrDefault(ad => ad.BookId == id);
+                var authorName = "";
+                if (authorDetail != null)
                 {
-                    book.BookId,
-                    book.BookTitle,
-                    book.UnitPrice,
-                    book.PublicationDate,
-                    Author = book.Authors.Select(author => author.AuthorName).ToList(),
-                    BookDiscountName = book.BookDiscounts.Select(discount => discount.DiscountName).FirstOrDefault()
-                })
-                .FirstOrDefault();
+                    var author = db.Authors.FirstOrDefault(a => a.AuthorId == authorDetail.AuthorId);
+                    if (author != null)
+                        authorName = author.AuthorName;
+                }
 
-            return View(datas);
+                // Get book discount information
+                var bookDiscountDetail = db.BookDiscountDetails.FirstOrDefault(bdd => bdd.BookId == id);
+                var bookDiscountName = "";
+                decimal bookDiscountAmount = 0m;
+                if (bookDiscountDetail != null)
+                {
+                    var bookDiscount = db.BookDiscounts.FirstOrDefault(bd => bd.BookDiscountId == bookDiscountDetail.BookDiscountId);
+                    if (bookDiscount != null)
+                    {
+                        bookDiscountName = bookDiscount.BookDiscountName;
+                        bookDiscountAmount = bookDiscount.BookDiscountAmount;
+                    }
+                }
+
+                // Create view model
+                var Model = new BooksCardViewModel
+                {
+                    BookId = book.BookId,
+                    BookTitle = book.BookTitle,
+                    UnitPrice = book.UnitPrice,
+                    PublicationDate = book.PublicationDate,
+                    AuthorId = authorDetail?.AuthorId ?? 0,
+                    AuthorName = authorName,
+                    BookDiscountName = bookDiscountName,
+                    BookDiscountAmount = bookDiscountAmount
+                };
+                return View(Model);
         }
+
     }
 }
