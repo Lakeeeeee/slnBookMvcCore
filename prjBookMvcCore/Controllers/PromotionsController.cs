1f78ca92096a8ff555545d9a4484551556f82dd5
stuu04 <b0132068@ems.niu.edu.tw>
2023-05-11 20:26:26 +0800

    Promotionse更新(Artical併入)

--- prjBookMvcCore/Controllers/PromotionsController.cs
+++ prjBookMvcCore/Controllers/PromotionsController.cs
@@ -1,28 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using prjBookMvcCore.Models;
 
 namespace prjBookMvcCore.Controllers
 {
     public class PromotionsController : Controller
     {
+        BookShopContext db = new();
+        public IActionResult Promotions註冊會員禮() { return View(); }
 
-        public IActionResult Promotions註冊會員禮()
+        public IActionResult Promotions會員() { return View(); }
+
+        public IActionResult Promotions促銷活動() { return View(); }
+
+        public IActionResult Promotions活動總覽圖()
         {
+            var datas = db.Articals.OrderByDescending(a => a.ArticalId).Select(a => a);
+            return View(datas);
+        }
+
+        public IActionResult Promotions活動總覽表()
+        {
+            var datas = db.Articals.OrderByDescending(a => a.ArticalId).Select(a => a);
+            return View(datas);
+         }
+
+        public IActionResult Promotions活動文章Detail(int? id)
+        {
+            if (id == null) { return RedirectToAction("Promotions活動已結束"); }
+            ViewBag.ArticalId = id;
             return View();
         }
 
-        public IActionResult Promotions會員()
-        {
-            return View();
-        }
+        public IActionResult Promotions活動已結束() { return View(); }
 
-        public IActionResult Promotions促銷活動()
-        {
-            return View();
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public IActionResult Index() { return View(); } //建置中
     }
 }
