51980aa3c9ba362bed3957574de91ca9bda44141
ShuyuWeng <wingy@apps.ntpc.edu.tw>
2023-05-12 13:34:41 +0800

    更新

--- prjBookMvcCore/Controllers/CategoryController.cs
+++ prjBookMvcCore/Controllers/CategoryController.cs
@@ -11,6 +11,7 @@ namespace prjBookMvcCore.Controllers
         public IActionResult 中文書()
         {
             var category = db.Categories.Select(b => b);
+            
             return View(category); 
         }
 
