11ddbea7370279fc29c8c59b6e1f5e39dcd729c6
ymtsai <blueallen2000@gmail.com>
2023-05-15 20:40:47 +0800

    update

---
+++ prjBookMvcCore/Models/DiscountType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace prjBookMvcCore.Models
+{
+    public partial class DiscountType
+    {
+        public int? DiscountTypeId { get; set; }
+        public string? DiscountTypeName { get; set; }
+    }
+}
