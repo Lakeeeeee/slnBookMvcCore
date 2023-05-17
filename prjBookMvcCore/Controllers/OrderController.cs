8d83326a93750bf1f0959e15aeda7148bd334dd9
hcjohn463 <hcjohn463@gmail.com>
2023-05-17 13:53:08 +0800

    delete useless order code

--- prjBookMvcCore/Controllers/OrderController.cs
+++ prjBookMvcCore/Controllers/OrderController.cs
@@ -13,31 +13,6 @@ namespace prjBookMvcCore.Controllers
         {
             return View();
         }
-        public string StoreShoppingCart(int bookID, int memberID, int actionID)
-        {
-            bool isSuccess = true;
-
-            var query = from ad in db.ActionDetials
-                        where ad.ActionId == actionID && ad.MemberId == memberID && ad.BookId == bookID
-                        select ad;
-            if (query.Count() != 0)
-            {
-                isSuccess = false;
-            }
-            else
-            {
-                ActionDetial newAd = new ActionDetial
-                {
-                    ActionId = actionID,
-                    BookId = bookID,
-                    MemberId = memberID,
-                };
-                db.ActionDetials.Add(newAd);
-                db.SaveChanges();
-            }
-            string jsonData = JsonConvert.SerializeObject(isSuccess);
-            return jsonData;
-        }
         public IActionResult ShoppingCart(int memberID)
         {
             List<CInformation> cartItems = new List<CInformation>();
