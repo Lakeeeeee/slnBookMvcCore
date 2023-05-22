e5da1086bb42ddcfe575deda9c6bbb84382dfac8
Lake <kmossrr5236@gmail.com>
2023-05-22 19:54:51 +0800

    update

--- prjBookMvcCore/Models/MemberManeger.cs
+++ prjBookMvcCore/Models/MemberManeger.cs
@@ -13,13 +13,14 @@ namespace prjBookMvcCore.Models
 {
     public class MemberManeger
     {
+        BookShopContext db = new BookShopContext();
         public void writeWelcomeLetter(Member receiver, BookShopContext content)
         {
             Message welcomeLetter = new Message()
             {
                 MessageContent = 
                 $"<p>Hi! {receiver.MemberName}!</p>" +
-                $"<p>歡迎加入我們的網路書店會員！我們非常高興你決定成為我們的一員，相信你將會在這裡找到許多有趣的閱讀體驗。</p>" +
+                $"<p>歡迎加入我們的網路書店會員！你現在是我們的  <span>{db.MemberLevels.Where(x=>x.LevelId== receiver.LevelId).FirstOrDefault().LevelName}</span>  會員了，相信你將會在這裡找到許多有趣的閱讀體驗。</p>" +
                 $"<p>作為我們的會員，你將享有一系列的優惠與福利，包括專屬的折扣碼、獨家的促銷活動、以及最新書籍的推薦和試讀機會。</p>" +
                 $"<p>此外，成為我們的會員還可以享有更多的方便和貼心的服務，例如訂閱通知、簡單的購物流程、多種付款方式、快速的物流配送等等。我們期待與你一同分享我們的閱讀愛好，並為你帶來更多的書香體驗。</p>",
                 MessageTypeId = 2,
