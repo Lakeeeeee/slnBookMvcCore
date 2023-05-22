724ecdac31663897ae082997f5559b1b2bb56953
stuu04 <b0132068@ems.niu.edu.tw>
2023-05-22 15:50:10 +0800

    ...

--- prjBookMvcCore/Models/OrderDiscount.cs
+++ prjBookMvcCore/Models/OrderDiscount.cs
@@ -7,16 +7,22 @@ namespace prjBookMvcCore.Models
     {
         public OrderDiscount()
         {
+            MemberLevels = new HashSet<MemberLevel>();
             OrderDiscountDetails = new HashSet<OrderDiscountDetail>();
+            Orders = new HashSet<Order>();
         }
 
         public int OrderDiscountId { get; set; }
+        public int DiscountTypeId { get; set; }
         public string OrderDiscountnName { get; set; } = null!;
         public string OrderDiscountDescprtion { get; set; } = null!;
         public int OrderDiscountCondition { get; set; }
         public decimal OrderDiscountAmount { get; set; }
-        public string OrderDiscountCode { get; set; } = null!;
+        public string? OrderDiscountCode { get; set; }
 
+        public virtual DiscountType DiscountType { get; set; } = null!;
+        public virtual ICollection<MemberLevel> MemberLevels { get; set; }
         public virtual ICollection<OrderDiscountDetail> OrderDiscountDetails { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
     }
 }
