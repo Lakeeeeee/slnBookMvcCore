8c0af156f0635b8e814a9fa707fc7496453a5f6d
ShuyuWeng <wingy@apps.ntpc.edu.tw>
2023-05-24 21:49:57 +0800

    Create 重置書本庫存.sql

---
+++ 重置書本庫存.sql
@@ -0,0 +1,192 @@
+
+
+
+-- 更新書本ID為1至10的庫存為0
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 0
+WHERE [BookID] BETWEEN 1 AND 10
+
+-- 更新書本ID為11至20的庫存為1
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 1
+WHERE [BookID] BETWEEN 11 AND 20
+
+-- 更新書本ID為21至30的庫存為2
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 2
+WHERE [BookID] BETWEEN 21 AND 30
+
+-- 更新書本ID為31至40的庫存為2
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 3
+WHERE [BookID] BETWEEN 31 AND 40
+
+-- 更新書本ID為41至50的庫存為3
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 3
+WHERE [BookID] BETWEEN 41 AND 50
+
+-- 更新書本ID為51至60的庫存為4
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 4
+WHERE [BookID] BETWEEN 51 AND 60
+
+-- 更新書本ID為61至70的庫存為5
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 5
+WHERE [BookID] BETWEEN 61 AND 70
+
+-- 更新書本ID為71至80的庫存為6
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 6
+WHERE [BookID] BETWEEN 71 AND 80
+
+-- 更新書本ID為81至90的庫存為7
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 7
+WHERE [BookID] BETWEEN 81 AND 90
+
+-- 更新書本ID為91至100的庫存為8
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 8
+WHERE [BookID] BETWEEN 91 AND 100
+
+-- 更新書本ID為101至110的庫存為9
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 9
+WHERE [BookID] BETWEEN 101 AND 110
+
+-- 更新書本ID為111至120的庫存為10
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 10
+WHERE [BookID] BETWEEN 111 AND 120
+
+-- 更新書本ID為121至130的庫存為0
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 0
+WHERE [BookID] BETWEEN 121 AND 130
+
+-- 更新書本ID為131至140的庫存為1
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 1
+WHERE [BookID] BETWEEN 131 AND 140
+
+-- 更新書本ID為141至150的庫存為2
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 2
+WHERE [BookID] BETWEEN 141 AND 150
+
+-- 更新書本ID為151至160的庫存為3
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 3
+WHERE [BookID] BETWEEN 151 AND 160
+
+-- 更新書本ID為161至170的庫存為4
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 4
+WHERE [BookID] BETWEEN 161 AND 170
+
+-- 更新書本ID為171至180的庫存為5
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 5
+WHERE [BookID] BETWEEN 171 AND 180
+
+-- 更新書本ID為181至190的庫存為6
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 6
+WHERE [BookID] BETWEEN 181 AND 190
+
+-- 更新書本ID為191至200的庫存為7
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 7
+WHERE [BookID] BETWEEN 191 AND 200
+
+-- 更新書本ID為201至210的庫存為0
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 0
+WHERE [BookID] BETWEEN 201 AND 210
+
+-- 更新書本ID為211至220的庫存為1
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 1
+WHERE [BookID] BETWEEN 211 AND 220
+
+-- 更新書本ID為221至230的庫存為2
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 2
+WHERE [BookID] BETWEEN 221 AND 230
+
+-- 更新書本ID為231至240的庫存為3
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 3
+WHERE [BookID] BETWEEN 231 AND 240
+
+-- 更新書本ID為241至250的庫存為4
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 4
+WHERE [BookID] BETWEEN 241 AND 250
+
+-- 更新書本ID為251至260的庫存為5
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 5
+WHERE [BookID] BETWEEN 251 AND 260
+
+-- 更新書本ID為261至270的庫存為6
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 6
+WHERE [BookID] BETWEEN 261 AND 270
+
+-- 更新書本ID為271至280的庫存為0
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 0
+WHERE [BookID] BETWEEN 271 AND 280
+
+-- 更新書本ID為281至290的庫存為1
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 1
+WHERE [BookID] BETWEEN 281 AND 290
+
+-- 更新書本ID為291至300的庫存為2
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 2
+WHERE [BookID] BETWEEN 291 AND 300
+
+-- 更新書本ID為301至310的庫存為3
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 3
+WHERE [BookID] BETWEEN 301 AND 310
+
+-- 更新書本ID為311至320的庫存為4
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 4
+WHERE [BookID] BETWEEN 311 AND 320
+
+-- 更新書本ID為321至330的庫存為5
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 5
+WHERE [BookID] BETWEEN 321 AND 330
+
+-- 更新書本ID為331至340的庫存為6
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 6
+WHERE [BookID] BETWEEN 341 AND 350
+
+-- 更新書本ID為351至360的庫存為0
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 0
+WHERE [BookID] BETWEEN 351 AND 360
+
+-- 更新書本ID為361至370的庫存為1
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 1
+WHERE [BookID] BETWEEN 361 AND 370
+
+-- 更新書本ID為371至380的庫存為2
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 2
+WHERE [BookID] BETWEEN 371 AND 380
+
+-- 更新書本ID為381至390的庫存為3
+UPDATE [dbo].[Books]
+SET [UnitInStock] = 3
+WHERE [BookID] BETWEEN 381 AND 390
